a128f4460d2b0a09e834605f1438a48aa2dbc681
ldiegoduran <105762258+ldiegoduran@users.noreply.github.com>
2023-08-13 11:13:18 -0600

    Finalización de semana 12 y 13

--- src/main/java/com/Tienda/controller/IndexController.java
+++ src/main/java/com/Tienda/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.Tienda.controller;
 
+import com.Tienda.service.ProductoService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
     
+    @Autowired
+    ProductoService productoService;
     @RequestMapping("/")
     public String page(Model model, HttpSession session) {
 //        String imagen = (String)session.getAttribute("usuarioImagen");
 //        model.addAttribute("avatar", imagen);
+        var listaProductos = productoService.getProductos(true);
+        model.addAttribute("productos", listaProductos);
         return "index";
     }
+    
+    
+    
     
 }
